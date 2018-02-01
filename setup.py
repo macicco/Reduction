bdab46df719fc74d5d8c9cd27ab2cd2de821cf8a
Walter Silva Martins Filho <walter@on.br>
2018-02-01 13:28:51 -0700

    including license and setup information

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='reduction',
+      version='0.1',
+      description='Python package created to make a astronomy reduction and analysis on astronomy FITS files.',
+      url='https://github.com/waltersmartinsf/Reduction',
+      author='Walter Silva Martins Filho',
+      author_email='walter@on.br',
+      license=' Creative Commons Attribution 4.0 International License',
+      packages=['reduction'],
+      zip_safe=False)
